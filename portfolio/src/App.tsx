fa0b59f8e523571c2ecd4dbc81db550858c6ef13
tushar16102004 <tusharbhanderi1610@gmail.com>
2026-07-12 14:31:48 +0530

    Add ScrollToTop component to reset window scroll position on route transitions

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Resume from "./pages/Resume";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/resume" element={<Resume />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </>
   );
 }
